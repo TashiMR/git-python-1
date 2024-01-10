0e887303c132ca242278b113f5f938e8e67121e2
Lab <lab@pc.com>
2024-01-10 20:31:51 +0500

    background added and exit setting

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 root = Tk()
 root.geometry("500x400")
-Label(root,text="Welcome",font=("",20,"bold")).pack()
-Button(root,text="Exit",command=exit).pack()
+root.config(bg="skyblue")
+Label(root,text="Welcome",font=("",20,"bold"),background="skyblue").pack()
+Button(root,text="Exit",command=exit).pack(side=BOTTOM,anchor="se",padx=20,pady=20)
 root.mainloop()
 #This is comment line
