c1134d5b75be5ae2170d7167d438b7dd73c3d13c
Lab <lab@pc.com>
2024-01-11 07:13:39 +0500

    commemt added-Both files changed

--- functions.py
+++ functions.py
@@ -1,2 +1,3 @@
 # This is fuctions file for main.py
 # 2nd comment
+# both files changed
--- main.py
+++ main.py
@@ -7,3 +7,4 @@ Label(root,text="Welcome",font=("",20,"bold"),background="skyblue").pack()
 Button(root,text="Exit",command=exit).pack(side=BOTTOM,anchor="se",padx=20,pady=20)
 root.mainloop()
 #This is comment line
+# both files changed
